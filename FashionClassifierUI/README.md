2935b1404bdc4902ce32a6e9c8f54a3571002524
Jacob Seiler <seiler.jacob1@gmail.com>
2022-12-15 21:03:37 -0500

    Added image selection

---
+++ FashionClassifierUI/README.md
@@ -0,0 +1,18 @@
+# Fashion Classifier UI
+
+Frontend for the model using [streamlit](https://streamlit.io/). All images are from [unsplash](https://unsplash.com/).
+
+## To run
+
+1. Set up a virtual environment using Python 3.10 (optional)
+1. Install requirements
+
+    ```bash
+    pip install -r requirements.txt
+    ```
+
+1. Run `main.py` using streamlit
+
+    ```bash
+    streamlit run main.py
+    ```
--- FashionClassifierUI/main.py
+++ FashionClassifierUI/main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit_image_select import image_select
 import keras
 from keras.models import Sequential
 from PIL import Image
@@ -12,6 +13,42 @@ def load_model() -> Sequential:
     return model
 
 def image_selector():
+    paths = [
+            "images/custom.jpg",
+            "images/t-shirt.jpg",
+            "images/pants.jpg",
+            "images/pullover.jpg",
+            "images/dress.jpg",
+            "images/coat.jpg",
+            "images/sandal.jpg",
+            "images/shirt.jpg",
+            "images/sneaker.jpg",
+            "images/bag.jpg",
+            "images/boots.jpg"
+        ]
+    
+    selection = image_select(
+        label="Sample images",
+        images=paths,
+        captions=["Upload Custom", *classes],
+        return_value="index"
+    )
+
+    img = None
+
+    if selection == 0:
+        file = st.file_uploader(label="Upload a custom image")
+
+        if file is not None:
+            bytes_data = file.read()
+            img = Image.open(BytesIO(bytes_data))
+    else:
+        img = Image.open(paths[selection])
+
+    if img is not None:
+        predictor(img)
+
+def image_uploader():
     uploaded_file = st.file_uploader(label='Upload a custom image')
 
     if uploaded_file is not None:
@@ -19,28 +56,31 @@ def image_selector():
 
         bytes_data = uploaded_file.read()
         image = Image.open(BytesIO(bytes_data))
-        st.image(image)
+        predictor(image)
 
-        with st.spinner("Predicting..."):
-            # Process image
-            image = remove(image) # Remove background
-            image = image.convert("L") # Make greyscale
-            image = image.resize((28, 28)) # Resize
+def predictor(image):
+    st.image(image)
 
-            # Convert to numpy array
-            pred_x = np.array([np.asarray(image)])
+    with st.spinner("Predicting..."):
+        # Process image
+        image = remove(image) # Remove background
+        image = image.convert("L") # Make greyscale
+        image = image.resize((28, 28)) # Resize
 
-            # Format input
-            pred_x = pred_x.reshape((pred_x.shape[0], 28, 28, 1))
-            pred_x = pred_x.astype("float32")
-            pred_x = pred_x / 255.0
-            pred_x = np.reshape(pred_x, (pred_x.shape[0], pred_x.shape[1], pred_x.shape[2], 1))
-            
-            # Run prediction on model
-            pred_y = np.argmax(model.predict(pred_x), axis=-1)
-            pred_y = classes[int(pred_y)]
-            st.header(pred_y)
-            st.balloons()
+        # Convert to numpy array
+        pred_x = np.array([np.asarray(image)])
+
+        # Format input
+        pred_x = pred_x.reshape((pred_x.shape[0], 28, 28, 1))
+        pred_x = pred_x.astype("float32")
+        pred_x = pred_x / 255.0
+        pred_x = np.reshape(pred_x, (pred_x.shape[0], pred_x.shape[1], pred_x.shape[2], 1))
+        
+        # Run prediction on model
+        pred_y = np.argmax(model.predict(pred_x), axis=-1)
+        pred_y = classes[int(pred_y)]
+        st.header(pred_y)
+        st.balloons()
 
 model = None
 classes = ["T-shirt/top", "Trouser", "Pullover", "Dress", "Coat", "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"]
